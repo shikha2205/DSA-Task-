e60c5b1899c43930c21c31aaa4151efe76815557
shikha2205 <shikha2022r2@gmail.com>
2024-11-05 10:19:34 +0530

    second

--- StringQuestions/isomorphicString.java
+++ StringQuestions/isomorphicString.java
@@ -1,5 +1,39 @@
 package StringQuestions;
-
+import java.util.HashMap;
 public class isomorphicString {
+    public static void main(String[] args) {
+        String s="egg";
+        String t="add";
+        System.out.println(isomorphic(s,t));
+
+    }
+    public static boolean isomorphic(String s,String t){
+        int n1=s.length();
+        int n2=t.length();
+        if(n1!=n2){
+            return false;
+        }
+        HashMap<Character,Character> map=new HashMap<>();
+        for(int i=0;i<n1;i++){
+            char schar=s.charAt(i);
+            char tchar=s.charAt(i);
+            if(map.containsKey(schar)){
+                if(map.get(schar)!=tchar){
+                    return false;
+                }
+
+            }
+            else{
+                if(map.containsValue(tchar)){
+                    return false;
+                }
+            }
+            map.put(schar,tchar);
+           
+
+        }
+        return true;
+
+    }
     
 }
